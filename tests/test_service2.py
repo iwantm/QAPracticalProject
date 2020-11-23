a59047bdc0a73e27c82fd38dc3a240dd0ab6183c
Iwan Moreton <me@iwanmoreton.com>
2020-11-23 20:00:52 +0000

    test service 2

---
+++ tests/test_service2.py
@@ -0,0 +1,20 @@
+from service2.app import app
+from flask_testing import TestCase
+from unittest.mock import patch
+from unittest.mock import patch
+
+from flask import url_for, request
+import requests
+
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+
+class TestService2(TestBase):
+    def test_random_country(self):
+        response = self.client.get(url_for('random_country'))
+        self.assert200(response)
+        self.assertIn(b'country_name', response.data)
+        self.assertIn(b'country_language', response.data)
