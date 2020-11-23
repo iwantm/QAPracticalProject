4fbe149e373aa62d1abaf48714b1cb8768f3d83d
Iwan Moreton <me@iwanmoreton.com>
2020-11-23 20:19:43 +0000

    test service4

---
+++ tests/test_service4.py
@@ -0,0 +1,25 @@
+from service4.app import app
+from flask_testing import TestCase
+from unittest.mock import patch
+from unittest.mock import patch
+import json
+from flask import url_for, request
+
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+
+class TestService2(TestBase):
+    def test_output(self):
+        with patch('requests.get') as g:
+            g.return_value.json.return_value = {
+                "names": ["Federica", "Desideria"]}
+            response = self.client.post(url_for('random_name'), data=json.dumps({
+                                        "country_language": "ita", "gender": "f"}), content_type='application/json')
+            self.assertIn(b'first_name', response.data)
+            self.assertIn(b'last_name', response.data)
+            self.assertIn(b'Federica', response.data)
+            self.assertIn(b'Desideria', response.data)
+            self.assert200
