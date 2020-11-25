92ccff581666426be1546194ed1e77153805d844
Iwan Moreton <me@iwanmoreton.com>
2020-11-25 15:53:00 +0000

    add scripts for auto deployment

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+ssh persongen-manager	 << EOF
+if [ -d "QAPracticalProject" ]; then
+    cd QAPracticalProject
+    git pull
+else
+    git clone https://github.com/iwantm/QAPracticalProject.git
+    cd QAPracticalProject/
+fi
+docker-compose pull
+docker stack deploy --compose-file docker-compose.yaml AnimalApp
+
+EOF
---
+++ scripts/test.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+sudo apt install python3 python3-pip python3-venv
+
+python3 -m venv testing-venv
+
+. testing-venv/bin/activate
+
+pip3 install -r tests/requirements.txt
+
+pytest --cov=service1
+pytest --cov=service2
+pytest --cov=service3
+pytest --cov=service4
+
+deactivate
+
+rm -rf testing-venv
