795c4804c720f273daf6c9d7b7d06236f1fbc384
Iwan Moreton <me@iwanmoreton.com>
2020-12-02 11:16:35 +0000

    hotfix-change order of commands

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
-sudo apt install python3 python3-pip -y
-pip install --user ansible
 source ~/.bashrc
-
+pip3 install --user ansible
 ansible-playbook -i ansible/Inventory ansible/playbook.yaml
 
