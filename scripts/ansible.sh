bc61e537acc183ce280a7b9f4961c1631e3c92f7
Iwan Moreton <me@iwanmoreton.com>
2020-12-02 11:11:04 +0000

    hotfix-change order of commands

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,11 +1,8 @@
 #! /bin/bash
 
 sudo apt install python3 python3-pip -y
-mkdir -p ~/.local/bin
-
 pip install --user ansible
-export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin"
-echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 source ~/.bashrc
+
 ansible-playbook -i ansible/Inventory ansible/playbook.yaml
 
